911df0ba49f9db9a035ff3c4850e9f9cbf513569
Mehdi-ehsani <metti.work@gmail.com>
2024-12-08 19:14:15 +0330

    config api use axios to config api

---
+++ src/configs/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { getCookie } from "../utils/cookie";
+
+const api = axios.create({
+	baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+	headers: {
+		"Content-Type": "application/json",
+	},
+});
+api.interceptors.request.use((request) => {
+	const token = getCookie("accessToken");
+	if (token) {
+		request.headers["Authorization"] = `Bearer ${token}`;
+	} 
+    return request;
+});
+api.interceptors.response.use((response) => {
+    return response;
+  });
+  
+  export default api;
