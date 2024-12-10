d0e53d76531e8ffa1d27f4eb69c9311a3e841705
Mehdi-ehsani <metti.work@gmail.com>
2024-12-10 23:15:37 +0330

    config image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	images: {
+		domains: ["localhost"],
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "6500",
+				pathname: "**",
+			},
+		],
+	},
+};
 
 export default nextConfig;
