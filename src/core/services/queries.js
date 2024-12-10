74ab7544fbb1515002739d9e20a0e149b45bc603
Mehdi-ehsani <metti.work@gmail.com>
2024-12-10 23:17:53 +0330

    Added useTours query

--- src/core/services/queries.js
+++ src/core/services/queries.js
@@ -1,10 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "@/core/configs/api";
 
-const useProfile = (pageNumber, name) => {
-	const queryFn = async (data) => {
+const useProfile = () => {
+	const queryFn = () => {
 		return api.get("user/profile");
 	};
 	return useQuery({ queryKey: ["profile"], queryFn });
 };
-export { useProfile };
+
+const useTours = () => {
+	const queryFn = () => {
+		return  api.get("tour");
+	};
+	return useQuery({ queryKey: ["tour"],  queryFn });
+};
+export { useProfile, useTours };
