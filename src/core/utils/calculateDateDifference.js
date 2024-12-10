d26182644e2d092fbd9435f4af96b6e7e079eac4
Mehdi-ehsani <metti.work@gmail.com>
2024-12-10 19:23:07 +0330

    Add function to calculate the number of tour days

---
+++ src/core/utils/calculateDateDifference.js
@@ -0,0 +1,14 @@
+const calculateDateDifference = (date1, date2) => {
+
+    const firstDate = new Date(date1);
+    const secondDate = new Date(date2);
+  
+    const timeDifference = Math.abs(firstDate - secondDate);
+  
+    const dayDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+  
+    return console.log(dayDifference);
+  };
+export default calculateDateDifference  
+  
+  
