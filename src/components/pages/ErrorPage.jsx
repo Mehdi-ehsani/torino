2cb39a68531fdf14ee96dc60a051146262bb6bb7
Mehdi-ehsani <metti.work@gmail.com>
2024-12-11 21:20:06 +0330

    Add error component to all routs

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -6,10 +6,10 @@ import errorImg from "../../assets/image/error.png"
 const ErrorPage = () => {
     const router = useRouter();
   return (
-    <div>
-        <div >
-          <h1></h1>
-          <button></button>
+    <div className="flex items-center justify-between w-full px-[174px]">
+        <div>
+          <h1 className="font-semibold text-[36px] text-[#282828]">اتصال با سرور برقرار نیست!</h1>
+          <h2 className="font-semibold text-[24px] text-[#282828]">لطفا بعدا دوباره امتحان کنید.</h2>
         </div>
         <Image src={errorImg} alt="Error"/>
     </div>
