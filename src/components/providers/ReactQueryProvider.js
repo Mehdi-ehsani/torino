1be900d8dc2067dbd803038fb1969decc044b04d
Mehdi-ehsani <metti.work@gmail.com>
2024-12-11 14:57:18 +0330

    Add state to query client

--- src/components/providers/ReactQueryProvider.js
+++ src/components/providers/ReactQueryProvider.js
@@ -1,10 +1,11 @@
 'use client'
+import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 
 const ReactQueryProvider = ({ children }) => {
-	const queryClient = new QueryClient();
+	const [queryClient] = useState(new QueryClient());
 	return (
 		<QueryClientProvider client={queryClient}>
 			{children}
