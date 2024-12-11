a1811ce038950c12747a2cfde43539adeda1fd72
Mehdi-ehsani <metti.work@gmail.com>
2024-12-11 20:00:43 +0330

    Add push to homePage btn

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -50,7 +50,7 @@ const Footer = () => {
 				</div>
 			</div>
 			<div className="w-full  text-center border-t border-[#00000020]">
-				<p className="text-[#00000050] leading-10 text-[15px] font-normal">
+				<p className="text-[#00000080] leading-10 text-[15px] font-normal">
 					کلیه حقوق این وب سایت متعلق به تورینو میباشد.
 				</p>
 			</div>
