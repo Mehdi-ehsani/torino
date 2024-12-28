4424b0d59a9966c121ae0590d7200050e7ced216
Mehdi-ehsani <metti.work@gmail.com>
2024-12-28 23:17:08 +0330

    config api for project

--- src/core/configs/api.js
+++ src/core/configs/api.js
@@ -1,21 +1,64 @@
 import axios from "axios";
-import { getCookie } from "../utils/cookie";
+
+import { getCookie, setCookie } from "../utils/cookie";
 
 const api = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-	headers: {
-		"Content-Type": "application/json",
-	},
-});
-api.interceptors.request.use((request) => {
-	const token = getCookie("accessToken");
-	if (token) {
-		request.headers["Authorization"] = `Bearer ${token}`;
-	}
-	return request;
-});
-api.interceptors.response.use((response) => {
-	return response;
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
+api.interceptors.request.use(
+  (request) => {
+    const accessToken = getCookie("accessToken");
+    if (accessToken) {
+      request.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const orginialRequest = error.config;
+    if (error.response.status === 401 && !orginialRequest._retry) {
+      orginialRequest._retry = true;
+
+      const res = await getNewTokens();
+      if (res?.response?.status === 201) {
+        setCookie("accessToken", res?.response?.data.accessToken );
+        setCookie("refreshToken", res?.response?.data.refreshToken );
+        return api(orginialRequest);
+      } else {
+        setCookie("accessToken", "");
+        setCookie("refreshToken", "");
+      }
+    }
+    return Promise.reject(error.response.data);
+  }
+);
+
 export default api;
+
+const getNewTokens = async () => {
+  const refreshToken = getCookie("refreshToken");
+  if (!refreshToken) return;
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}auth/refresh-token`,
+      {
+        refreshToken,
+      }
+    );
+    return { response };
+  } catch (error) {
+    return { error };
+  }
+};
