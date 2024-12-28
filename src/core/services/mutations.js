fd030aff1ccfcf92d8f867b23e81c8d5a5b31d37
Mehdi-ehsani <metti.work@gmail.com>
2024-12-28 23:18:52 +0330

    Add all mutations

--- src/core/services/mutations.js
+++ src/core/services/mutations.js
@@ -1,5 +1,6 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import api from "../configs/api";
+import { setCookie } from "../utils/cookie";
 
 const useLogin = () => {
 	const mutationFn =  (data) => {
@@ -9,9 +10,38 @@ const useLogin = () => {
 };
 
 const useSendOtp = () => {
+	const queryClient = useQueryClient()
 	const mutationFn =  (data) => {
 	 return api.post("auth/check-otp", data)
 	};
+	const onSuccess = (data) => {
+		setCookie("accessToken", data?.data?.accessToken);
+		setCookie("refreshToken", data?.data?.refreshToken);
+		queryClient.invalidateQueries({ queryKey: ["profile"] });
+	  };
+	return useMutation({ mutationFn , onSuccess});
+};
+const useSendToBasket = (id) => {
+	const mutationFn =  (data) => {
+	 return api.put(`basket/${id}`, data)
+	};
 	return useMutation({ mutationFn });
 };
-export { useLogin , useSendOtp };
+const useSendOrder = () => {
+	const mutationFn =  (data) => {
+	 return api.post("order", data)
+	};
+	return useMutation({ mutationFn });
+};
+const usePutProfile = () => {
+	const queryClient = useQueryClient();
+
+	const mutationFn =  (data) => {
+	 return api.put("user/profile" , data)
+	};
+	const onSuccess = (data) => {
+		queryClient.invalidateQueries({ queryKey: ["profile"] });
+	  };
+	return useMutation({ mutationFn , onSuccess});
+};
+export { useLogin , useSendOtp , useSendOrder , useSendToBasket , usePutProfile};
