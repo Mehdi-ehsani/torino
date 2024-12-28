5c630d8d2bcb64457d8f14933b93b0e74f9d73b1
Mehdi-ehsani <metti.work@gmail.com>
2024-12-28 23:17:41 +0330

    Add tour page

--- src/app/tour/page.js
+++ src/app/tour/page.js
@@ -1,7 +1,22 @@
-import ToursPage from "@/components/pages/ToursPage";
+import { serverFetch } from "@/core/services/http";
+import Tour from "../../components/common/Tour";
+import SearchBox from "@/components/common/SearchBox";
+import Spinner from "@/components/templates/spinner";
 
-export default function Tours() {
-  return (
-    <ToursPage/>
-  );
+async function Tours({searchParams}) {
+	const data = await serverFetch("tour", searchParams, { cache: "no-store" });
+	
+	if(!data) return <Spinner/>
+	return (
+		<div className="lg:px-[126px] px-[31px] mb-10 lg:mb-16">
+			<SearchBox/>
+			<h1 className="text-[20px] md:text-[32px] font-medium my-8">همه تور ها</h1>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4  gap-7 ">
+				{data?.map((tour) => (
+					<Tour key={tour.id} tour={tour} />
+				))}
+			</div>
+		</div>
+	);
 }
+export default Tours;
