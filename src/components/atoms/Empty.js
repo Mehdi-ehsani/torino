aec28c3993b37cc6ec891e91dfed899fe04363d1
Mehdi-ehsani <metti.work@gmail.com>
2024-12-28 23:12:42 +0330

    Add empty component for when transaction is empty

---
+++ src/components/atoms/Empty.js
@@ -0,0 +1,9 @@
+const Empty = ({txt}) => {
+  return (
+    <div className="flex py-[50px] items-center justify-center">
+       <h1 className="font-medium text-2xl">{txt}</h1>
+    </div>
+  )
+}
+
+export default Empty
