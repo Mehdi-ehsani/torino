1cbb166e8061f40860993254d13b39342bde973a
Mehdi-ehsani <metti.work@gmail.com>
2024-12-28 23:14:54 +0330

    Add tour card to tours page

--- src/components/common/Tour.jsx
+++ src/components/common/Tour.jsx
@@ -1,18 +1,14 @@
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 
 import calculateDateDifference from "@/core/utils/calculateDateDifference";
 import changeVehicleName from "@/core/utils/changeVehicleName";
 import formatNumber from "@/core/utils/formatNumber";
 import getPersianMonth from "@/core/utils/gePersianMonth";
+import GoTourDetailsBtn from "../atoms/GoTourDetailsBtn";
 
 const Tour = ({tour}) => {
-    const router = useRouter()
-    const goTourDetails = () => {
-      router.push(`/tour/${tour.id}`)
-    }
 	return (
-		<div onClick={goTourDetails} className="border border-[#00000012] rounded-[10px]" key={tour.id}>
+		<div  className="border border-[#00000012] rounded-[10px]" key={tour.id}>
 			<div className=" relative w-full h-[159px]">
 				<Image
 					className="w-auto h-auto"
@@ -21,6 +17,7 @@ const Tour = ({tour}) => {
 					src={tour.image}
 					alt="tour img"
 				/>
+
 			</div>
 			<h2 className="text-[22px] font-medium m-2">{tour.title}</h2>
 			<p className="mx-2 mb-1 text-[15px] text-[#28282870]">
@@ -29,9 +26,7 @@ const Tour = ({tour}) => {
 				{changeVehicleName(tour.fleetVehicle)} - هتل 3 ...
 			</p>
 			<div className="flex justify-between items-center border-t border-[#00000012] p-2 ">
-				<button className="rounded px-5 bg-primery text-lg text-white">
-					رزرو
-				</button>
+				<GoTourDetailsBtn id={tour.id} />
 				<p className="text-[#009ECA]">
 					{formatNumber(tour.price)}{" "}
 					<span className="text-[#28282880] text-sm">تومان</span>
