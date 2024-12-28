6e5c00c060a365fedcc9e6830c86a8f5693574ea
Mehdi-ehsani <metti.work@gmail.com>
2024-12-28 23:19:08 +0330

    Add all queries

--- src/core/services/queries.js
+++ src/core/services/queries.js
@@ -8,9 +8,9 @@ const useProfile = () => {
 	return useQuery({ queryKey: ["profile"], queryFn });
 };
 
-const useTours = () => {
+const useTours = (query) => {
 	const queryFn = () => {
-		return  api.get("tour");
+		return  api.get(`tour${query}`);
 	};
 	return useQuery({ queryKey: ["tour"],  queryFn });
 };
@@ -20,4 +20,23 @@ const useTourDetail = (id) => {
 	};
 	return useQuery({ queryKey: ["tour/datail"],  queryFn });
 };
-export { useProfile, useTours , useTourDetail };
+const useUserTours = () => {
+	const queryFn = () => {
+		return  api.get("user/tours");
+	};
+	return useQuery({ queryKey: ["user/tours"],  queryFn });
+};
+const useUserTransactions = () => {
+	const queryFn = () => {
+		return  api.get("user/transactions");
+	};
+	return useQuery({ queryKey: ["user/transactions"],  queryFn });
+};
+const useGetBasket = () => {
+	const queryFn = () => {
+		return  api.get("basket");
+	};
+	return useQuery({ queryKey: ["basket"],  queryFn });
+};
+
+export { useProfile, useTours , useTourDetail ,useUserTours , useUserTransactions , useGetBasket };
