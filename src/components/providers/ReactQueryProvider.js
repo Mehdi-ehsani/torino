a4d8624099d7251eaea89b398899bd26a2e40964
Mehdi-ehsani <metti.work@gmail.com>
2024-12-28 23:16:26 +0330

    wrap react query to app

--- src/components/providers/ReactQueryProvider.js
+++ src/components/providers/ReactQueryProvider.js
@@ -1,15 +1,12 @@
 'use client'
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 
 const ReactQueryProvider = ({ children }) => {
 	const [queryClient] = useState(new QueryClient());
 	return (
 		<QueryClientProvider client={queryClient}>
-			{children}
-			<ReactQueryDevtools/>
+			{children}		
         </QueryClientProvider>
 	);
 };
