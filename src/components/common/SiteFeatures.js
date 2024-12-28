c59698bb38d33c25ecd02dc4bbcbdbbfa048308c
Mehdi-ehsani <metti.work@gmail.com>
2024-12-28 23:26:14 +0330

    Add responsive

--- src/components/common/SiteFeatures.js
+++ src/components/common/SiteFeatures.js
@@ -6,22 +6,22 @@ import Image from "next/image";
 
 const SiteFeatures = () => {
 	return (
-		<main className="grid grid-cols-3 items-start justify-between gap-28 py-9 px-[126px]">
-			<div className="flex items-center gap-4">
+		<main className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 items-start justify-between gap-28 py-9 px-[31px] lg:px-[126px]">
+			<div className="flex items-center gap-4 mx-auto md:mx-0">
                 <Image src={persentImg} alt="icon" />
                 <div>
                     <h1 className="font-medium text-[26px] text-[#282828]">بصرفه ترین قیمت</h1>
                     <h2 className="font-light text-[16px] mt-1 text-[#282828]">بصرفه ترین و ارزان ترین قیمت تور را از ما بخواهید.</h2>
                 </div>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 mx-auto md:mx-0">
                 <Image src={messageImg} alt="icon" />
                 <div>
                     <h1 className="font-medium text-[26px] text-[#282828]">پشتیبانی</h1>
                     <h2 className="font-light text-[16px] mt-1 text-[#282828]">پشتیبانی و همراهی 24 ساعته در تمامی مراحل سفر شما.</h2>
                 </div>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 mx-auto md:mx-0">
                 <Image src={heartImg} alt="icon" />
                 <div>
                     <h1 className="font-medium text-[26px] text-[#282828]">رضایت کاربران</h1>
