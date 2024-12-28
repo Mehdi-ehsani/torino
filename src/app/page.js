240f55005c11299ee5f8390a8435416b352d75af
Mehdi-ehsani <metti.work@gmail.com>
2024-12-28 23:56:18 +0330

    Add all homepage component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,21 @@
-import Homepage from "@/components/pages/Homepage";
+import SiteFeatures from "@/components/common/SiteFeatures";
+import Header from "../components/common/Header";
+import TorinoDetail from "../components/common/TorinoDetail";
+import Tours from "./tour/page";
+import ImageSlider from "@/components/common/ImageSlider";
 
 export default function Home() {
-	return <Homepage />;
+	return (
+		<div>
+			<Header />
+			<h1 className="sm:text-3xl text-[16px] text-center mt-6 font-semibold text-[#595959]">
+				<span className="text-[#28A745]">تورینو</span> برگزار کننده بهترین تور
+				های داخلی و خارجی
+			</h1>
+			<Tours />
+			<TorinoDetail />
+			<ImageSlider/>
+			<SiteFeatures/>
+		</div>
+	);
 }
